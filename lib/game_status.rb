9bb2c5400d9af25c131b3c3327a2c65eea21b4a4
Morgan Matney <morgan.matney89@gmail.com>
2020-06-27 20:49:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,47 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # First column
+  [1,4,7], # Second column
+  [2,5,8], # Third column
+  [0,4,8], # First diagonal
+  [2,4,6], # Second diagonal
+]
+
+# won? method
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+  position_1 = board[win_combo[0]] #takes 1st index of 1st array
+  position_2 = board[win_combo[1]]
+  position_3 = board[win_combo[2]]
+
+  position_1 == position_2 && position_2 == position_3 &&
+  position_taken?(board, win_combo[0]) #delineates its not an open space
+  end
+end
+
+#full? method
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+#draw? method
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+#over? method
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+#winner method
+def winner(board)
+  won?(board) && board[won?(board)[0]]
+end
